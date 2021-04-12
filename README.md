c0d55cf824b7ecda216296e0d3c844247f5d2bad
Stefano Bieler <stefanobieler@gmail.com>
2021-04-12 15:36:06 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Modern CPP
+
+This is just a project full of libraries I've made at the direction of following of a modern C++ course listed
+on udemy.
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "config.hpp"
+#include <iostream>
+
+
+int main() {
+
+    std::cout << PROJECT_NAME << " " << PROJECT_VERSION << std::endl;
+
+
+
+    return 0;
+}
